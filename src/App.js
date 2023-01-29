48fbdbc28ef2d8c5a2c965d4002bfa9a787e6c10
Muhammad Gumilang Tamasakti <gumilangtamasakti@gmail.com>
2023-01-29 12:57:38 +0700

    style: create header component and add routes to landingpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./assets/scss/style.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "pages/LandingPage";
 
 function App() {
-  return <div className="App">a</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Button from "elements/Button";
+
+export default function IconText() {
+  return (
+    <Button className="brand-text-iicon" href="" type="link">
+      Stay <span className="text-gray-900">Cation.</span>
+    </Button>
+  );
+}
